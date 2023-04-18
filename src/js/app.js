0f1a385d8ffac3ba2852ae04ca1a0636ec0a0e45
MateusRSdev <mateusRSdev@gmail.com>
2023-04-18 10:05:35 -0300

    apanhando para promisses

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,36 @@
 
+$(document).ready( () => {
+    $("#alt-A").on("click", e => {
+        console.log(e);
+    })
+    
+    const espera = () =>{
+        return new Promise(resolve => {
+            $.ajax({
+                type: "get",
+                url: "src/js/countryInfo.json",
+                data: "",
+                dataType: "",
+                success: function (response) {
+                    resolve(response)
+                },
+                error: function(e){
+                    console.log(e);
+                }
+            
+            });
+        })
+    }
+    
+    const out = async ()=>{
+        const x = await espera()
+        return x;
+    }
+  
+    resolve(out())
 
-console.log(paises);
+    
+
+
+    
+})

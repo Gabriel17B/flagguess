85ce159d762701e574b2bb410b772c8ea710ae66
MateusRSdev <mateusRSdev@gmail.com>
2023-04-26 09:05:27 -0300

    testando...

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,5 @@
 $('[data-toggle="popover"]').popover()
 
 function iniciarTreino(num){
-    window.URL = "jogo.html?gamemode=questionary&bamd="+ num;
+    window.location.href = "jogo.html?gamemode=questionary&band="+ num;
 }

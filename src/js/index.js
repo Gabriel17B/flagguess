7df7630756c9908aa1b6ccd23d8541dc275bb919
MateusRSdev <mateusRSdev@gmail.com>
2023-04-26 09:49:32 -0300

    testando...

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,11 @@
-$('[data-toggle="popover"]').popover()
+$('#treino').popover()
+$('.ranking').popover()
 
 function iniciarTreino(num){
     window.location.href = "jogo.html?gamemode=questionary&band="+ num;
+}
+
+function setContinent(){
+    console.log($("#selCont").val())
+    sessionStorage.setItem("continent",$("#selCont").val())
 }
